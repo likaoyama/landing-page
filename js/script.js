cdafe1fb5faab7a85bf771af6ab23d51b65ccea1
likaoyama <lika.oyama@hotmail.com>
2021-07-18 20:48:42 -0300

    Subindo arquivos

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+    let pronto = `<p>Pronto! Já foi cadastrado!</p>`
+    content.innerHTML = pronto
+})
